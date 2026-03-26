9d683084b3b4da94720df3f11ed7949e163ad8b5
Mayank <mayank1312.raj@gmail.com>
2026-03-27 00:27:25 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const app = require("./app")
+const connectToDatabase = require("./config/database")
+
+
+connectToDatabase()
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
